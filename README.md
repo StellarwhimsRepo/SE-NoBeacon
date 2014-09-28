ac74eaa8c43c54208d2a6ce8dfce074cac2df82f
Tostito <tostitogaming@live.com>
2014-09-28 15:16:23 -0500

    files uploaded

---
+++ README.md
@@ -0,0 +1,15 @@
+SE-NoBeacon
+===============
+**INTENDED FOR DEDICATED SERVERS
+
+The author of this script is not responsible for any damage to the game save or the machine it is run on.
+
+Grid removal if no Beacon is found.
+
+This powershell script is used by changing the variables at the top of the 'ps1' file to match your server settings.
+
+The default settings will delete object grids from the game if those grids have no beacon.
+
+Be sure to shut your space engineers server down before running this script!!!
+
+Use the windows shortcut provided to run. Be sure the shortcut and the script file are in the same folder.
---
+++ SE_NoBeacon.ps1
@@ -0,0 +1,19 @@
+﻿
+    $filePath = 'your save path here\SANDBOX_0_0_0_.sbs'
+    
+    [xml]$myXML = Get-Content $filePath
+    $ns = New-Object System.Xml.XmlNamespaceManager($myXML.NameTable)
+    $ns.AddNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
+    
+    
+    #delete grid if no beacon.
+    $nodes = $myXML.SelectNodes("//SectorObjects/MyObjectBuilder_EntityBase[(@xsi:type='MyObjectBuilder_CubeGrid')]"  , $ns) 
+    ForEach($node in $nodes){
+        $beaconcount = $node.SelectNodes("CubeBlocks/MyObjectBuilder_CubeBlock[@xsi:type='MyObjectBuilder_Beacon']", $ns).count
+            IF($beaconcount -eq 0){
+                 $node.ParentNode.RemoveChild($node)
+            }
+    }
+
+
+    $myXML.Save($filePath)
