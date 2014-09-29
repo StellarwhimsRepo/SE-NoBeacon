ffb05940149bd233df65a3064150002805ebb083
Tostito <tostitogaming@live.com>
2014-09-28 19:34:49 -0500

    code changes
    Added ignore if rotor top, piston top, or wheel.

--- SE_NoBeacon.ps1
+++ SE_NoBeacon.ps1
@@ -1,17 +1,25 @@
 ﻿
     $filePath = 'your save path here\SANDBOX_0_0_0_.sbs'
+
+    # ===== only change the above value
     
     [xml]$myXML = Get-Content $filePath
     $ns = New-Object System.Xml.XmlNamespaceManager($myXML.NameTable)
     $ns.AddNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
     
     
-    #delete grid if no beacon.
+    #delete grid if no beacon, then if no wheels, rotor, piston pieces.
     $nodes = $myXML.SelectNodes("//SectorObjects/MyObjectBuilder_EntityBase[(@xsi:type='MyObjectBuilder_CubeGrid')]"  , $ns) 
     ForEach($node in $nodes){
         $beaconcount = $node.SelectNodes("CubeBlocks/MyObjectBuilder_CubeBlock[@xsi:type='MyObjectBuilder_Beacon']", $ns).count
             IF($beaconcount -eq 0){
-                 $node.ParentNode.RemoveChild($node)
+                $rotorcount = $node.SelectNodes("CubeBlocks/MyObjectBuilder_CubeBlock[@xsi:type='MyobjectBuilder_MotorRotor']", $ns).count
+                $pistoncount = $node.SelectNodes("CubeBlocks/MyObjectBuilder_CubeBlock[@xsi:type='MyobjectBuilder_PistonTop']", $ns).count
+                $wheelcount = $node.SelectNodes("CubeBlocks/MyObjectBuilder_CubeBlock[@xsi:type='MyobjectBuilder_Wheel']", $ns).count
+                $ignoretotal = $rotorcount + $pistoncount + $wheelcount
+                IF($ignoretotal -eq 0){
+                    $node.ParentNode.RemoveChild($node)
+                }
             }
     }
 
