f55a178f651fa78721c1f51512a66a0c3f1eaa3d
Tostito <tostitogaming@live.com>
2014-11-05 22:21:51 -0600

    added logging
    added logging

--- SE_NoBeacon.ps1
+++ SE_NoBeacon.ps1
@@ -1,13 +1,21 @@
 ﻿
     $filePath = 'your save path here\SANDBOX_0_0_0_.sbs'
+    $deletedlogs = "yourlogs path here\Admin Logs\Audits\deleted\"
 
     # ===== only change the above value
+
+    $CurrentDateTime = Get-Date -Format "MM-dd-yyyy_HH-mm"
+    $deletedfilename = "Owned_Audit_" +$CurrentDateTime+ ".log"
+    $deletedpath = $deletedLogs + $deletedfilename
     
     [xml]$myXML = Get-Content $filePath
     $ns = New-Object System.Xml.XmlNamespaceManager($myXML.NameTable)
     $ns.AddNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
 
-    
+    New-Item -path $deletedpath -type file
+
+    Add-Content -path $deletedpath -Value "No-Beacon Check ... "
+    Add-Content -path $deletedpath -Value "[  ]"
     
     #delete grid if no beacon, then if no wheels, rotor, piston pieces.
     $nodes = $myXML.SelectNodes("//SectorObjects/MyObjectBuilder_EntityBase[(@xsi:type='MyObjectBuilder_CubeGrid')]"  , $ns) 
@@ -21,6 +29,8 @@
                 $advrotorcount = $node.SelectNodes("CubeBlocks/MyObjectBuilder_CubeBlock[@xsi:type='MyObjectBuilder_MotorAdvancedRotor']", $ns).count
                 $ignoretotal = $ignoretotal + $rotorcount + $pistoncount + $wheelcount + $advrotorcount
                 IF($ignoretotal -eq 0){
+                    Add-Content -path $deletedpath -Value "[$($node.DisplayName)] Deleted for no beacons"
+                    Add-Content -path $deletedpath -Value "[  ]"
                     $node.ParentNode.RemoveChild($node)
                 }
             }
